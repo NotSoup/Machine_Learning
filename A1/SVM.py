99a03de50204d18fcff179b89bcd1fe6d31a68c3
Angelo Petrolino <apetrolino1990@gmail.com>
2024-09-15 19:44:50 -0400

    Windows test commit

--- A1/SVM.py
+++ A1/SVM.py
@@ -4,6 +4,7 @@ from sklearn.metrics import accuracy_score, classification_report
 
 
 def SVM(X, y):
+    
     # Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 

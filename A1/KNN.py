f1b85ee5f15faa46f19044b29216fdfd389daba7
Angelo Petrolino <apetrolino1990@gmail.com>
2024-09-23 23:05:03 -0400

    done

--- A1/KNN.py
+++ A1/KNN.py
@@ -1,80 +1,98 @@
-from sklearn.model_selection import train_test_split, cross_val_score, learning_curve, validation_curve
+from sklearn.model_selection import learning_curve, validation_curve
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def KNN(X, y):
+def KNN(X_train, X_test, y_train, y_test, ax, metric):
 
-    #####
-    # Reqired-to-be-tuned hyperparameters: K
-    #   +2 more
-    #####
+    knn = KNeighborsClassifier(
+        n_neighbors=50,
+        metric='canberra',
+        weights='distance'
+    )
 
-    # Split the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=90210)
+    ###################### Learning Curve ######################
+    train_sizes, train_scores, valid_scores = learning_curve(
+        knn, 
+        X_train, 
+        y_train, 
+        cv=5, 
+        scoring=metric, 
+        n_jobs=-1
+    )
 
-    # Create the k-NN classifier
-    k = 1  # Number of neighbors to use
-    knn = KNeighborsClassifier(n_neighbors=k)
-
-    # Perform cross-validation (5-fold)
-    cv_scores = cross_val_score(knn, X_train, y_train, cv=3)
-
-    # # Print the cross-validation scores for each fold
-    # print(f"Cross-Validation Scores: {cv_scores}")
-
-    # Print the average cross-validation score
-    print(f"KNN Avg CV Score:  {np.mean(cv_scores):.2f}")
-
-    # Train the classifier
-    knn.fit(X_train, y_train)
-
-    # Make predictions on the test set
-    y_pred = knn.predict(X_test)
-
-    # Evaluate the accuracy of the model
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"KNN Test Accuracy: {accuracy:.2f}\n")
-
-    # Learning Curve: Model performance with varying training set sizes
-    train_sizes, train_scores, valid_scores = learning_curve(knn, X_train, y_train, cv=4, scoring='accuracy', n_jobs=-1)
-
-    # Compute average training and validation scores across the n-folds
     train_mean = np.mean(train_scores, axis=1)
     valid_mean = np.mean(valid_scores, axis=1)
 
-    # Plotting the Learning Curve
-    plt.figure(figsize=(8, 6))
+    plt.clf()
+    plt.figure(figsize=(6,6)) # or (7,6)
     plt.plot(train_sizes, train_mean, label='Training Accuracy', color='blue', marker='o')
     plt.plot(train_sizes, valid_mean, label='Validation Accuracy', color='red', marker='o')
-    plt.title('Learning Curve')
+    plt.title('Learning Curve for k-NN')
     plt.xlabel('Training Set Size')
-    plt.ylabel('Accuracy')
+    plt.ylabel(metric)
     plt.legend(loc='best')
     plt.grid()
-    plt.show()
+    plt.savefig('./plots/r_knn_learn.png', dpi=300)
 
-    # # Validation Curve: Model performance with varying number of neighbors
-    # param_range = np.arange(1, 21)  # Trying k values from 1 to 20
-    # train_scores_vc, valid_scores_vc = validation_curve(knn, X_train, y_train, param_name='n_neighbors',
-    #                                                     param_range=param_range, cv=5, scoring='accuracy', n_jobs=-1)
+    ###################### Validation Curve ######################
+    # # param = 'n_neighbors'
+    # # param_range = np.arange(1, 200, 1)                                                              # 10        # 50
+    # # param = 'metric'
+    # # param_range = ['braycurtis','canberra','chebyshev',
+    # #         'correlation','cosine','euclidean', 'manhattan','minkowski', 'sqeuclidean']             # manhattan # canberra
+    # param = 'weights'
+    # param_range = ['uniform', 'distance']                                                           # distance  # distance
 
-    # # Compute average training and validation scores for each k value
-    # train_mean_vc = np.mean(train_scores_vc, axis=1)
-    # valid_mean_vc = np.mean(valid_scores_vc, axis=1)
+    # train_scores, valid_scores = validation_curve(
+    #     knn, 
+    #     X_train, 
+    #     y_train, 
+    #     param_name=param,                                              
+    #     param_range=param_range, 
+    #     cv=5, 
+    #     scoring=metric, 
+    #     n_jobs=-1
+    # )
 
-    # # Plotting the Validation Curve
-    # plt.figure(figsize=(8, 6))
-    # plt.plot(param_range, train_mean_vc, label='Training Accuracy', color='blue')
-    # plt.plot(param_range, valid_mean_vc, label='Validation Accuracy', color='red')
-    # plt.title('Validation Curve for k-NN')
-    # plt.xlabel('Number of Neighbors (k)')
-    # plt.ylabel('Accuracy')
+    # train_mean = np.mean(train_scores, axis=1)
+    # valid_mean = np.mean(valid_scores, axis=1)
+
+    # # Weighting
+    # print(train_mean)
+    # print(valid_mean)
+
+    # plt.clf()
+    # plt.figure(figsize=(6,6)) # or (7,6)
+    # plt.plot(param_range, train_mean, label='Training Accuracy', color='blue')
+    # plt.plot(param_range, valid_mean, label='Validation Accuracy', color='green')
+
+    # # # For distance metric
+    # # plt.figure(figsize=(9,9))
+    # # plt.bar(param_range, train_mean, label='Training Accuracy', color='blue')
+    # # plt.bar(param_range, valid_mean, label='Validation Accuracy', color='green')
+    # # plt.xticks(rotation=45, ha='right')
+    # plt.ylim([0.86, 0.93])
+    # # plt.xlabel('Distance Metrics')
+    # # # plt.title('Weighting Metrics for k-NN')
+    # # plt.title(f'Distance Metrics for k-NN')
+
+    # plt.title(f'Validation Curve for k-NN {param}')
+    # plt.xlabel(f'{param}')
+    # plt.ylabel(metric)
     # plt.legend(loc='best')
     # plt.grid()
-    # plt.show()
+    # plt.savefig(f'./plots/r_knn_{param}.png', dpi=300)
 
+    # The test
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
 
-    return accuracy
+    if metric == 'accuracy':
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"KNN Test Accuracy: {accuracy:.2f}")
+    if metric == 'recall_macro':
+        recall = recall_score(y_test, y_pred, average='macro')
+        print(f"KNN Test Recall: {recall:.2f}")
--- A1/main.py
+++ A1/main.py
@@ -5,157 +5,71 @@ from MLP import NN
 import pandas as pd
 import statistics
 from timeit import default_timer as timer
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
 start = timer()
-data_dir = "./data/"
+data_dir = "..\data\\"
 
-# # Std Dev: 5.4      [46, 58, 56, 54]
-# # Runtime: 3.6
-# # Instances: 2044
-# dataset_name = data_dir + "/heart/processed.cleveland.data"
-# df = pd.read_csv(dataset_name)
-# df = df.drop([87, 166, 192, 287, 302, 266])    # 'thal' & 'ca' with ? values
-# X = df.drop(["num"], axis=1)
-# y = df["num"]
+######################## OBESITY DATASET ##########################
+dataset_name = "ObesityDataSet_raw_and_data_sinthetic"
+df = pd.read_csv(data_dir + dataset_name + ".csv")
 
-# # Std Dev: 9.3    [61, 76, 61, 77]
-# # Runtime: 70
-# # Instances: 4425
-# # Features: 36
-# dataset_name = data_dir + "student_dropout_success.csv"
-# df = pd.read_csv(dataset_name, sep=";")
-# X = df.drop(["Target"], axis=1)
-# y = df["Target"]
+df_dumm = pd.get_dummies(
+    df, 
+    columns=['Gender', 
+             'family_history_with_overweight', 
+             'FAVC', 
+             'CAEC', 
+             'SMOKE', 
+             'SCC', 
+             'CALC', 
+             'MTRANS' 
+    ]
+)
 
-# # Std Dev: 9.3    [24, 24, 27, 26]
-# # Runtime: 70
-# # Instances: 4425
-# # Features: 36
-# dataset_name = data_dir + "abalone.data"
-# df = pd.read_csv(dataset_name)
-# X = df.drop(["Rings"], axis=1)
-# X = pd.get_dummies(X)
-# y = df["Rings"]
+X = df_dumm.drop(columns=['NObeyesdad'])
+y = df['NObeyesdad']
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
 
-# # Std Dev: 
-# # Runtime: 70
-# # Instances: 4425
-# # Features: 36
-# dataset_name = data_dir + "housing.csv"
-# df = pd.read_csv(dataset_name)
-# X = df.drop(["Rings"], axis=1)
-# X = pd.get_dummies(X)
-# y = df["Rings"]
+######################## RICE DATASET ##########################
+# dataset_name = "Rice_Cammeo_Osmancik"
+# df = pd.read_csv(data_dir + dataset_name + ".csv")
+# X = df.drop(["Class"], axis=1)
+# y = df["Class"]
 
-# # Std Dev: 6.8
-# # Runtime: 463
-# # Instances: 4600
-# # Features: 58
-# dataset_name = data_dir + "spambase.data"
-# df = pd.read_csv(dataset_name)
-# X = df.iloc[:, :-1]
-# y = df.iloc[:, -1]
+# # Convert rice class strings to numeric
+# label_encoder = LabelEncoder()
+# y = label_encoder.fit_transform(y)
 
-# # Std Dev: 8.1      [58, 56, 42, 61]
-# # Runtime: 15
-# # Instances: 1483
-# # Features: 10
-# dataset_name = data_dir + "yeast.data"
-# df = pd.read_csv(dataset_name, delim_whitespace=True)
-# X = df.iloc[:, 1:-1]
-# y = df.iloc[:, -1]
+######################## PRE-PROCESS ##########################
+# Metrics (precision, recall, F1 score)
+# metric = 'accuracy'
+# metric = 'f1'
+metric = 'recall_macro'
 
+# # Normalize/scale features (for stable gradient descent, etc)
+# scaler = StandardScaler()
+# X = scaler.fit_transform(X)
 
-######################## NUMERICAL / BINARY Y ############################
-# Std Dev: 9.5     [88, 94, 73, 93]
-# Runtime: 10
-# Instances: 3810
-# Features: 7
-dataset_name = data_dir + "Rice_Cammeo_Osmancik.arff"
-df = pd.read_csv(dataset_name)
-X = df.drop(["Class"], axis=1)
-y = df["Class"]
+# Split dataset for test/train
+X_train, X_test, y_train, y_test = train_test_split(
+    X, 
+    y, 
+    test_size=0.2, 
+    random_state=90210,
+)
 
-# https://archive.ics.uci.edu/dataset/545/rice+cammeo+and+osmancik
-####################################################################
+fig, ax = plt.subplots(2, 4, figsize=(23,14))
 
-# # Std Dev: 5.9
-# # Runtime: 16
-# # Instances: 2111
-# dataset_name = data_dir + "ObesityDataSet_raw_and_data_sinthetic.csv"
-# df = pd.read_csv(dataset_name)
-# X = df.drop(["NObeyesdad"], axis=1)
-# X = pd.get_dummies(X)
-# y = df["NObeyesdad"]
+######################## LEARN ##########################
+KNN(X_train, X_test, y_train, y_test, ax, metric)
+SVM(X_train, X_test, y_train, y_test, ax, metric)
+NN(X_train, X_test, y_train, y_test, ax, metric)
+# BoostDT(X_train, X_test, y_train, y_test, ax, metric)
 
-# # Std Dev: 
-# # Runtime: 
-# # Instances: 
-# dataset_name = data_dir + "optdigits.tra"
-# df = pd.read_csv(dataset_name)
-# dataset_name = data_dir + "optdigits.tes"
-# df2 = pd.read_csv(dataset_name)
-# df.columns = [None] * len(df.columns)
-# df2.columns = [None] * len(df2.columns)
-# df = pd.concat([df, df2])
-# X = df.iloc[:, :-1]
-# y = df.iloc[:, -1]
-
-# # Std Dev: 4.6
-# # Runtime: 27
-# # Instances: 2310
-# # Features: 19
-# dataset_name = data_dir + "segmentation.data"
-# df = pd.read_csv(dataset_name)
-# y = df.index
-# X = df.reset_index(drop=True)
-
-# # Std Dev: 4.6
-# # Runtime: 27
-# # Instances: 2310
-# # Features: 19
-# dataset_name = data_dir + "agaricus-lepiota.data"
-# df = pd.read_csv(dataset_name)
-# X = df.iloc[:, 1:]
-# y = df.iloc[:, 0]
-# X = pd.get_dummies(X)
-
-# # Std Dev: 4.6
-# # Runtime: 27
-# # Instances: 2310
-# # Features: 19
-# dataset_name = data_dir + "sat.trn"
-# df = pd.read_csv(dataset_name, delimiter=" ")
-# X = df.iloc[:, :-1]
-# y = df.iloc[:, -1]
-
-# from datasets import load_dataset
-
-# df = load_dataset("mstz/heart_failure", split="train")
-# df = df.to_pandas()
-# X = df.drop(["is_dead"], axis=1)
-# y = df["is_dead"]
-
-# # Std Dev: 4.3
-# # Runtime: 5.2
-# # Instances: 1729
-# dataset_name = data_dir + "car.data"
-# df = pd.read_csv(dataset_name)
-# X = df.drop(["class"], axis=1)
-# y = df["class"]
-# X = pd.get_dummies(X)
-
-#####
-# Performance metrics (BESIDES ERROR): precision, recall, F1 score
-#####
-
-dataset_diff = []
-dataset_diff.append(100 * KNN(X, y))
-dataset_diff.append(100 * SVM(X, y))
-dataset_diff.append(100 * NN(X, y))
-dataset_diff.append(100 * BoostDT(X, y))
-
-print(f"Std Dev of dataset: {statistics.stdev(dataset_diff)}")
-end = timer()
-print(f"Runtime: {end - start}")
+print(f"Runtime: {timer() - start}")
+# plt.savefig(f'./plots/full_suite_{dataset_name}.png', dpi=300)
